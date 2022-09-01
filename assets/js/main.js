9c23da8cbef45c610525b00289e6ab1c5e822207
Dinh Quang Huy <dinhquanghuy0809@gmail.com>
2022-09-02 00:16:21 +0700

    Update issue reponsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ const navSlider = () => {
         link.style.animation = '';
       } else {
         console.log(index);
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +2}s`;
+        link.style.animation = `navLinkFade 0.1s ease forwards ${index / 7 +2}s`;
       }
     });
   });
